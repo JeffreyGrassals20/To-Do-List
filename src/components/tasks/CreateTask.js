b1d3efc3a69a570bb682e1ed3b0f39d03045c0f4
Jeffrey <slassarg20@gmail.com>
2022-07-05 19:04:05 -0400

    [FEAT]: Added date and createdAt to tasksLists

--- src/components/tasks/CreateTask.js
+++ src/components/tasks/CreateTask.js
@@ -1,28 +1,64 @@
-import React from 'react'
-import { Button } from '@mui/material';
+import React, { useState } from 'react'
+import { Button, Input, TextField } from '@mui/material';
 import { ref, set } from 'firebase/database';
 import { db } from '../../firebase/firebase';
 import { uid } from 'uid';
 
 
+const CreateTask = () => {
 
-const addData = () => {
-    const date = Date.now()
+const [tittle, setTittle] = useState('')
+const [body, setBody] = useState('')
+const [dueDate, setDueDate] = useState('')
+
+const addData = (tittle, body,_dueDate) => {
+    
+    const date = new Date(Date.now()).toLocaleString('en-US')
     const taskId = uid();
     set(ref(db,`${'toDoList/'} ${taskId}`),{
         id: taskId,
-        tittle: 'Enviar Correo',
-        body: 'Enviar Correo A fulano el loco',
-        date: date.toLocaleString('en-US')
+        tittle: tittle,
+        body: body,
+        createdAt: date,
+        date: _dueDate
     }).then(()=> {
         alert('Task Created')
     });
 }
 
-const CreateTask = () => {
-  return (
+const handleTittle = (e) =>{
+    setTittle(e.target.value);
+}
+
+const handleBody = (e) => {
+    setBody(e.target.value); 
+}
+
+const handleSubmit = () => {
+    addData(tittle,body,dueDate)
+}
+const handleDueDate= (e)=> {
+    setDueDate(e.target.value)
+}
+return (
     <>
-        <Button onClick={addData}> Crear Task </Button>
+        
+        <Input id='tittle' placeholder='tittle' onChange={handleTittle}/><br/>
+        <Input id='body' placeholder='body' onChange={handleBody}/><br/>
+
+        <form style={{marginTop: 80}} noValidate>
+            <TextField
+                id="date"
+                label="Fecha"
+                type="datetime-local"
+                InputLabelProps={{
+                shrink: true,
+                }}
+                onChange= {handleDueDate}
+            />
+        </form>
+        
+        <Button onClick={handleSubmit}> Crear Task </Button>
     </>
   )
 }
---
+++ src/components/tasks/GetTasks.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { db } from '../../firebase/firebase';
+
+const GetTasks = () => {
+  return (
+    <div>GetTasks</div>
+  )
+}
+
+export default GetTasks

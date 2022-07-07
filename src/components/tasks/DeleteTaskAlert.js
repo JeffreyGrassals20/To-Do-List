1bafba558584e4237f737e9a145590abfd0da31b
Jeffrey <slassarg20@gmail.com>
2022-07-07 01:27:36 -0400

    [FIX]: Edit task Component styles fixed

--- src/components/tasks/DeleteTaskAlert.js
+++ src/components/tasks/DeleteTaskAlert.js
@@ -1,9 +1,13 @@
-import { Alert, Button } from '@mui/material'
+import { Alert, Button, Typography } from '@mui/material'
 import React from 'react'
 import { remove, ref} from 'firebase/database';
 import { db } from '../../firebase/firebase';
-
+import { useStyles } from '../../styles/DeleteTask';
+import { Delete } from '@mui/icons-material';
+import { Box } from '@mui/system';
 const DeleteTaskAlert = ({id, onClose}) => {
+
+    const style = useStyles()
 
     const deleteTask = (id)=>{
         remove(ref(db,`${'toDoList/'} ${id}`)).then(()=> {
@@ -15,10 +19,30 @@ const DeleteTaskAlert = ({id, onClose}) => {
 
 
     return (
-    <div>
+    <div className={style.modal}>
         <Alert variant='filled' severity='error'>
-            Shure?
-            <Button onClick={() => deleteTask(id)}> Delete</Button>
+            <Box>
+                <Typography>
+                    Delete this Task?
+                </Typography>
+            </Box>
+            <Box mt={2}>
+                <Button 
+                size='small'
+                variant='contained' 
+                color='primary'
+                startIcon={<Delete/>} 
+                onClick={() => deleteTask(id)}> Delete
+                </Button>
+
+                <Button size='small' 
+                variant='outlined'
+                onClick={() => onClose()}
+                color='inherit'
+                style={{marginLeft: 100}}>
+                    Close
+                </Button>
+            </Box>
         </Alert>
     </div>
   )
